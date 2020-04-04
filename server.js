31f59db78b0efd62740613c368e80035fae49fa5
robertdbjohnson <joh16151@byui.edu>
2020-04-04 04:24:46 -0600

    initial version

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ function getPerson(req, res) {
     console.log("Retrieving person with id: ", id);
 
     getPersonFromDb(id, function(error, result) {
-
+/*
         if (error || result == null || result.length != 1) {
             res.status(500).json({success:false, data: error});
         } else {
             res.json(result[0]);
         }
-
+*/
         console.log("Back from the database with result:", result);
         res.json(result);
         res.end();
@@ -41,14 +41,13 @@ function getPersonFromDb(id, callback) {
     var sql = "SELECT id, first, last, phone, email, date, time FROM persons WHERE id = $1::int";
     var param = [id];
     pool.query(sql, param, function(err, result) {
-        if (err) {
+        /*if (err) {
             console.log("An error occured with the DB");
             console.log(err);
             callback(err, null);
-        }
+        }*/
         console.log("Found DB result: " + JSON.stringify(result.rows));
         callback(null, result.rows);
-    })
-
+    }) 
 }
 
