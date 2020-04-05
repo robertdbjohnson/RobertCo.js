b3a4ab15e29a4f8204d250072e3936b3e3f0e178
robertdbjohnson <joh16151@byui.edu>
2020-04-04 22:06:51 -0600

    initial version

--- models/appointmentsModel.js
+++ models/appointmentsModel.js
@@ -14,7 +14,7 @@ function getAllAppointments(callback) {
 
             var result = {
                 appointments: [
-                    {first:"Robert", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"},
+                    {first:"Roberttt", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"},
                     {first:"Heidi", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"},
                     {first:"Joseph", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"}]};
             callback(result);
