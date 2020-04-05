3f698fee9b31dd26188b746bafdbcd7b8fcc1c5d
robertdbjohnson <joh16151@byui.edu>
2020-04-04 20:41:41 -0600

    initial version

--- public/appointments-clientside.js
+++ public/appointments-clientside.js
@@ -1,3 +1,17 @@
 function LoadllAppointments() {
     console.log("Loading All Appointments...")
+    var appointment = $("#appointment").val();
+    console.log("Appointment: " + appointment);
+
+    $.get("/appointments", {appointment:appointment}, function(data) {
+		console.log("Back from the server with:");
+		console.log(data);
+
+		for (var i = 0; i < data.list.length; i++) {
+			var appointment = data.list[i];
+
+			$("#ulAppointments").append("<li>" + appointment.first + " " + appointment.last + ": " + appointment.phone + "</li>");
+		}
+
+	})
 }
