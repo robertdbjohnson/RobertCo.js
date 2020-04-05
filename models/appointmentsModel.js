dfa316357e5a77b5497daad8d03fda504a6f3090
robertdbjohnson <joh16151@byui.edu>
2020-04-04 21:55:42 -0600

    initial version

--- models/appointmentsModel.js
+++ models/appointmentsModel.js
@@ -3,14 +3,22 @@ const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 
 function getAllAppointments(callback) {
-    var result = {
-        appointments: [
-            {first:"Robert", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"},
-            {first:"Heidi", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"},
-            {first:"Joseph", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"}
-        ]
-    }
-    callback(result);
+    var sql = "SELECT id, first, last, phone, email, date, time FROM persons";
+    pool.query(sql, function(err, db_result) {
+        if (err) {
+            throw err;
+        } else {
+            console.log("Back from the DB with: ")
+            console.log(db_result);
+
+            var result = {
+                appointments: [
+                    {first:"Robert", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"},
+                    {first:"Heidi", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"},
+                    {first:"Joseph", last:"Johnson", phone:"111-222-3333", email:"rj@gmail.com", date:"05-02-2020", time:"12:00pm"}]};
+            callback(result);
+        }
+    });
 }
 
 function postNewAppointment(name, callback) {
